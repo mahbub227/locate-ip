68749a549ecee6c5d344822ffab02a425a8b5e33
Mahbub Ul Alam <mahbub.ruet123098@gmail.com>
2023-06-23 17:10:02 +0600

    URL changed

--- src/services/getHostInfo.ts
+++ src/services/getHostInfo.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export const getHostInfo = async (ip: string) => {
     const { VITE_SODAN_API_KEY } = import.meta.env;
-    const { VITE_SODAN_URL } = import.meta.env;
+    const { VITE_SODAN_BASE_URL } = import.meta.env;
     try {
-        const url = `${VITE_SODAN_URL}/shodan/host/${ip}?key=${VITE_SODAN_API_KEY}`;
+        const url = `${VITE_SODAN_BASE_URL}/shodan/host/${ip}?key=${VITE_SODAN_API_KEY}`;
         const { data, status } = await axios.get(url);
         if (status === 200) return data;
         else throw new Error('Something went wrong!');
